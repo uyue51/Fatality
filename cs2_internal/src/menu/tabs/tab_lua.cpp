6e7355adf0c545ea159bf66fc568f11de9a6b644
meguru <932463735@qq.com>
2025-05-16 19:10:47 +0800

    Add files via upload

---
+++ cs2_internal/src/menu/tabs/tab_lua.cpp
@@ -0,0 +1,17 @@
+#include <menu/menu.h>
+#include <gui/controls/selectable_script.h>
+#include <shellapi.h>
+#include "game/cfg.h"
+#include "game/game.h"
+#include "menu/macros.h"
+
+using namespace evo::gui;
+using namespace evo::gui::helpers;
+using namespace evo::ren;
+
+namespace menu::group
+{
+	
+
+
+}
